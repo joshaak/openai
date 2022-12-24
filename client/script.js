39c61af1b2a266ed0dcd2fe83e2767d0cf2ab982
sharukh <mohammedsharukh987@gmail.com>
2022-12-24 15:37:21 +0530

    second commit

--- client/script.js
+++ client/script.js
@@ -84,7 +84,7 @@ const handleSubmit = async (e) => {
   // messageDiv.innerHTML = "..."
   loader(messageDiv);
 
-  const response = await fetch("https://localhost:5000/", {
+  const response = await fetch("https://openai-3ls2.onrender.com", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
--- server/server.js
+++ server/server.js
@@ -42,5 +42,5 @@ app.post("/", async (req, res) => {
 });
 
 app.listen(5001, () =>
-  console.log("Server is running on port http://localhost:5001")
+  console.log("Server is running on port https://openai-3ls2.onrender.com")
 );
